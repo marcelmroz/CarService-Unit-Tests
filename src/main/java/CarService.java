60a9067ace120cf1977dcc7f06d03ed18c3d56b3
Marcel Mroz <mmroz.marcel@gmail.com>
2023-10-10 11:50:10 +0200

    carSerivce update

--- src/main/java/CarService.java
+++ src/main/java/CarService.java
@@ -1,27 +1,28 @@
 public class CarService {
+    public CarService() {
+    }
 
     public boolean analyzeCarByParams(Integer treadThickness, Integer fuelUsage, Integer carMillage) {
-
         boolean millageNull = null == carMillage;
         boolean fuelUsageNull = null == fuelUsage;
         boolean treadThicknessNull = null == treadThickness;
-        if (millageNull || fuelUsageNull || treadThicknessNull) {
-
+        if (!millageNull && !fuelUsageNull && !treadThicknessNull) {
+            boolean isMillageCorrect = this.isCorrect(carMillage, ParamValues.CAR_MILLAGE);
+            boolean isFuelUsageCorrect = this.isCorrect(fuelUsage, ParamValues.FUEL_USAGE);
+            boolean isTreadThicknessCorrect = this.isCorrect(treadThickness, ParamValues.TREAD_THICKNESS);
+            return isMillageCorrect && isFuelUsageCorrect && isTreadThicknessCorrect;
+        } else {
             throw new IllegalArgumentException("Car details can not be null");
         }
-        boolean isMillageCorrect = isCorrect(carMillage, ParamValues.CAR_MILLAGE);
-        boolean isFuelUsageCorrect = isCorrect(fuelUsage, ParamValues.FUEL_USAGE);
-        boolean isTreadThicknessCorrect = isCorrect(treadThickness, ParamValues.TREAD_THICKNESS);
-        return isMillageCorrect && isFuelUsageCorrect && isTreadThicknessCorrect;
     }
 
     private boolean isCorrect(Integer value, ParamValues validValues) {
-
         if (value < 0) {
             throw new IllegalArgumentException("Incorrect value");
+        } else {
+            Integer maxValue = validValues.getMaxValue();
+            Integer minValue = validValues.getMinValue();
+            return value > minValue && value < maxValue;
         }
-        Integer maxValue = validValues.getMaxValue();
-        Integer minValue = validValues.getMinValue();
-        return value >= minValue && value < maxValue;
     }
 }
